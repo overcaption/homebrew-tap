f360e5f74d50418a50aed16149147ca563228c88
overcaption <overcaption@gmail.com>
2026-08-12 18:13:48 +0000

    overcaption 0.3.5

--- Casks/overcaption.rb
+++ Casks/overcaption.rb
@@ -1,6 +1,6 @@
 cask "overcaption" do
-  version "0.3.4"
-  sha256 "d426150d8c806dea4ba9238434ff2c3fe7d0c8f9817e7fe644b401d2310990db"
+  version "0.3.5"
+  sha256 "d8ce14551a066f2bf900cf88f2b2b35f02ba763402d64e7408ec6ce14ea31d18"
 
   url "https://github.com/overcaption/overcaption/releases/download/v#{version}/Overcaption-#{version}-mac-arm64.dmg"
   name "Overcaption"
